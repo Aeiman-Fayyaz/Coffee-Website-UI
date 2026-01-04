1c2283bd4367d6ec2782efd20ed2ef4eebd52169
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2026-01-05 02:21:46 +0500

    code update

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import React, { useRef } from "react";
 import "./App.css";
 import logo from "./assets/logo.png";
 import heroImg from "./assets/hero.jpg";
+import cappucino from "./assets/Capuccino.jpg";
+import espresso from "./assets/espresso.jpg";
 
 export default function App() {
   return (
     <>
       {/* NAVBAR SECTION START */}
-      <nav className="bg-neutral-primary fixed w-full z-20 top-0 start-0">
+      <nav className="bg-white fixed w-full z-20 top-0 start-0">
         <div className="max-w-7xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
             href="#"
@@ -179,18 +181,21 @@ export default function App() {
       </nav>
       {/* NAVBAR SECTION END */}
       {/* HERO SECTION START */}
-      <section className="body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <section className="body-font lg:mx-38">
+        <div className="container flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg\:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-7xl text-3xl font-bold hero-heading text-black">
               Best coffee
             </h1>
             <br className="hidden lg:inline-block" />
-            <span className="sub-heading text-[#964B00]">Make <span className="text-black hero-text">your day</span> great <span className="text-black hero-text">with our</span> coffee!</span>
+            <span className="sub-heading text-[#964B00]">
+              Make <span className="text-black hero-text">your day</span> great{" "}
+              <span className="text-black hero-text">with our</span> coffee!
+            </span>
             <p className="mb-8 leading-relaxed text-black">
-              Lorem ipsum dolor sit amet, consectetur odipiscing elit, <br /> sed do
-              eiusmod tempor incididunt ut labore et dolore <br /> magna aliqua. Ut
-              enim ad minim veniam
+              Lorem ipsum dolor sit amet, consectetur odipiscing elit, <br />{" "}
+              sed do eiusmod tempor incididunt ut labore et dolore <br /> magna
+              aliqua. Ut enim ad minim veniam
             </p>
             <div className="flex justify-center">
               <button className="inline-flex text-white bg-black border-0 py-2 px-6 focus:outline-none hover:bg-transparent hover:border-2 hover:border-black hover:text-black  rounded text-lg cursor-pointer">
@@ -198,9 +203,9 @@ export default function App() {
               </button>
             </div>
           </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 lg:ms-28">
             <img
-              className="object-cover object-center rounded"
+              className="object-cover object-center rounded hero-img"
               alt="hero"
               src={heroImg}
             />
@@ -208,6 +213,218 @@ export default function App() {
         </div>
       </section>
       {/* HERO SECTION END */}
+      {/* COFFEE SECTION START */}
+      <section class="container px-5 mx-auto body-font">
+        <div class="text-center mb-20">
+          <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-[#964B00] mb-4">
+            Best Coffee
+          </h1>
+          <p class="text-black leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+            We Serves You Our Best & Original Coffee, Loves the taste.
+          </p>
+        </div>
+        <div className="container px-5 mx-auto">
+          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
+            <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 shrink-0">
+              <img src={cappucino} className="h-20 w-20 rounded-full" alt="" />
+            </div>
+            <div className="grow sm:text-left text-center mt-6 sm:mt-0">
+              <h2 className="text-[#964B00] text-lg title-font font-extrabold mb-2">
+                Cappucino
+              </h2>
+              <p className="leading-relaxed text-base">Best Serves</p>
+            </div>
+          </div>
+          <div className="flex items-center lg:w-3/5 mx-auto sm:flex-row flex-col">
+            <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+              <img src={espresso} className="h-20 w-20 rounded-full" alt="" />
+            </div>
+            <div className="grow sm:text-left text-center mt-6 sm:mt-0">
+              <h2 className="text-[#964B00] text-lg title-font font-extrabold mb-2">
+                Espresso
+              </h2>
+              <p className="leading-relaxed text-base">Black coffee lovers</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* COFFEE SECTION END */}
+      {/* FOOTER SECTION START */}
+      <footer class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+          <div class="w-64 shrink-0 md:mx-0 mx-auto text-center md:text-left md:mt-0 mt-10">
+            <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+              </svg>
+              <span class="ml-3 text-xl">Tailblocks</span>
+            </a>
+            <p class="mt-2 text-sm text-gray-500">
+              Air plant banjo lyft occupy retro adaptogen indego
+            </p>
+          </div>
+          <div class="grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
+            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+                CATEGORIES
+              </h2>
+              <nav class="list-none mb-10">
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                </li>
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                </li>
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                </li>
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                </li>
+              </nav>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+                CATEGORIES
+              </h2>
+              <nav class="list-none mb-10">
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                </li>
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                </li>
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                </li>
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                </li>
+              </nav>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+                CATEGORIES
+              </h2>
+              <nav class="list-none mb-10">
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                </li>
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                </li>
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                </li>
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                </li>
+              </nav>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+              <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+                CATEGORIES
+              </h2>
+              <nav class="list-none mb-10">
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                </li>
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                </li>
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                </li>
+                <li>
+                  <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                </li>
+              </nav>
+            </div>
+          </div>
+        </div>
+        <div class="bg-gray-100">
+          <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+            <p class="text-gray-500 text-sm text-center sm:text-left">
+              © 2020 Tailblocks —
+              <a
+                href="https://twitter.com/knyttneve"
+                rel="noopener noreferrer"
+                class="text-gray-600 ml-1"
+                target="_blank"
+              >
+                @knyttneve
+              </a>
+            </p>
+            <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+              <a class="text-gray-500">
+                <svg
+                  fill="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                </svg>
+              </a>
+              <a class="ml-3 text-gray-500">
+                <svg
+                  fill="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                </svg>
+              </a>
+              <a class="ml-3 text-gray-500">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                  <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                </svg>
+              </a>
+              <a class="ml-3 text-gray-500">
+                <svg
+                  fill="currentColor"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="0"
+                  class="w-5 h-5"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="none"
+                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                  ></path>
+                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                </svg>
+              </a>
+            </span>
+          </div>
+        </div>
+      </footer>
+      {/* FOOTER SECTION END */}
     </>
   );
 }
